ca6d7cbedd97e01b641726b12f034531e083aae4
Dmitriy Umerenkov <dmitriy@umerenkov.ru>
2015-11-20 21:03:56 +0300

    First commin

---
+++ src/Chapter1/Ex1.java
@@ -0,0 +1,16 @@
+package Chapter1;
+
+public class Ex1 {
+int i;
+char c;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ex1 Example=new Ex1();
+		
+		System.out.println("i="+Example.i);
+		System.out.println("c="+Example.c);
+		
+	}
+
+}
